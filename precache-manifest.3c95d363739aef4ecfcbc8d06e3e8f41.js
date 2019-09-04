7ea8defc43de16f9bd2904ab2155b36cd2eeeed4
simonjsuh <simon.j.suh@gmail.com>
2019-09-04 14:47:29 -0700

    Updates

---
+++ precache-manifest.3c95d363739aef4ecfcbc8d06e3e8f41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6f2677ce85cbc5c699bea5d51dbea5",
+    "url": "/Twitch-API-app/index.html"
+  },
+  {
+    "revision": "1a5ddbab8dcde45f06cc",
+    "url": "/Twitch-API-app/static/css/main.6dd39fd9.chunk.css"
+  },
+  {
+    "revision": "a342f55681d483244a74",
+    "url": "/Twitch-API-app/static/js/2.bf52ad71.chunk.js"
+  },
+  {
+    "revision": "1a5ddbab8dcde45f06cc",
+    "url": "/Twitch-API-app/static/js/main.b0c6af77.chunk.js"
+  },
+  {
+    "revision": "f54a77c694541dc36a9b",
+    "url": "/Twitch-API-app/static/js/runtime~main.37d4a5fe.js"
+  }
+]);
